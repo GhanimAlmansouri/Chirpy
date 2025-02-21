274568c080360aa55c396f390edfee7a351d4f07
GhanimAlmansouri <ghanim.almansouri@gmail.com>
2025-02-21 12:52:51 +0000

    2: Routing

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,14 +13,38 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (api *apiConfig) hits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	x := fmt.Sprintf("Hits: %d", api.fileServerHits.Load())
+	w.Write([]byte(x))
+}
+
+func (api *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	api.fileServerHits.Store(0)
+	w.Write([]byte("Hits Reset Successfully"))
+}
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	mux := http.NewServeMux()
 	var server http.Server
 	server.Addr = "localhost:8080"
 	server.Handler = mux
-
-	mux.HandleFunc("/healthz", healthHandler)
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	var apiCfg apiConfig
+	mux.HandleFunc("GET /healthz", healthHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.hits)
+	mux.HandleFunc("POST /reset", apiCfg.reset)
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	log.Fatal(server.ListenAndServe())
 
 }
